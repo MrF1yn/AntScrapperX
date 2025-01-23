5075040140f457048eb62a983c62dc150f1add4d
Dibyajyoti Dey <dibyajyoti49@outlook.com>
2025-01-23 14:12:13 +0530

    added quora to flask server

--- ScrapperDriver.py
+++ ScrapperDriver.py
@@ -3,7 +3,6 @@ import subprocess
 
 app = Flask(__name__)
 
-
 @app.route('/run_scrapper', methods=['GET'])
 def run_scrapper():
     scrapper_name = request.args.get('scrapper')
@@ -12,11 +11,12 @@ def run_scrapper():
         result = subprocess.run(['python', 'AmazonScrapper.py'], capture_output=True, text=True)
     elif scrapper_name == 'flipkart':
         result = subprocess.run(['python', 'FlipkartScrapper.py'], capture_output=True, text=True)
+    elif scrapper_name == 'quora':
+        result = subprocess.run(['python', 'QuoraScrapper.py'], capture_output=True, text=True)
     else:
         return jsonify({'error': 'Invalid scrapper name'}), 400
 
     return jsonify({'output': result.stdout, 'error': result.stderr})
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
