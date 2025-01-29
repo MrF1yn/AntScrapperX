5c23c0d07fb85208313768e7ce208c554ace2eaf
Dibyajyoti Dey <dibyajyoti49@outlook.com>
2025-01-29 12:57:44 +0530

    driver file changes

--- Driver.py
+++ Driver.py
@@ -173,8 +173,8 @@ if __name__ == "__main__":
     redis_thread.start()
 
     # Example usage
-    # push_to_redis("phone-numbers-csv.csv", redis_lists_numbers)
-    # push_to_redis("emails-csv.csv", redis_lists_emails)
+    push_to_redis("phone-numbers-csv.csv", redis_lists_numbers)
+    push_to_redis("emails-csv.csv", redis_lists_emails)
 
     # Keep the main thread alive
     while True:
